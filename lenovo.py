ea77580085a9a20430ff6efa936bbe0d59c30078
Daniel Walsh <dangwalsh@gmail.com>
2021-07-26 13:30:02 -0400

    added exception handling

--- lenovo.py
+++ lenovo.py
@@ -2,29 +2,49 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
+import pandas
+import sys
 
 PAUSE_TIME = 0.5
 
-def get_model(text):
-    field = driver.find_element_by_css_selector('input')
+def get_description(element, text, pause=0.1):
+    description = ''
+    field = element.find_element_by_css_selector('input')
     field.send_keys(text[:4])
-    time.sleep(PAUSE_TIME)
-    return driver.find_element_by_css_selector('div.tt-content-up').text
+    time.sleep(pause)
 
-def get_model_number(text):
-    return text[:4]
+    try:
+        description = element.find_element_by_css_selector('div.tt-content-up').text
+    except:
+        print(sys.exc_info()[0])
+        input('Hit Enter to continue...')
+
+    for _ in range(4):
+        field.send_keys(Keys.BACKSPACE)
+
+    return description
 
 
+# Read Data
+df = pandas.read_csv('computerslist.csv', delimiter=';')
+model_numbers = df.Model
+
+
+# Read Web
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--headless")
-
 chrome_options.binary_location = "C:\Program Files\Google\Chrome Beta\Application\chrome.exe"
 driver = webdriver.Chrome(executable_path="drivers/chromedriver.exe", options=chrome_options)
-
 driver.get('https://pcsupport.lenovo.com/us/en/')
 time.sleep(PAUSE_TIME)
-number = get_model_number(get_model('20J7S0QM00'))
 
-pass
+# Append Data
+descriptions = []
+for mn in model_numbers:
+    descriptions.append(get_description(driver, mn, PAUSE_TIME))
+df['Description'] = descriptions
+
+# Append File
+df.to_csv('computerslist.csv')
